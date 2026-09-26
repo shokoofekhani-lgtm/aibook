5ab277f9ed6cc82ee57affe6a0bedb9d157d5a48
Shokufeh <shokoofekhani45@gmail.com>
2026-09-26 11:55:08 +0330

    adding python stats

---
+++ stats.py
@@ -0,0 +1,18 @@
+def file_stats(filename):
+    with open(filename, "r", encoding="utf-8") as file:
+        text = file.read()
+
+    lines = text.splitlines()
+    words = text.split()
+    characters = len(text)
+
+    return {
+        "lines": len(lines),
+        "words": len(words),
+        "characters": characters
+    }
+
+
+stats = file_stats("example.txt")
+
+print(stats)
